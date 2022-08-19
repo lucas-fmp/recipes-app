9fd54995ff3ebfe582ca60305201e9fb71750d28
Carlos Arthur <arthurandrade918@gmail.com>
2022-08-19 18:51:53 -0300

    Header Feito

--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from './MyContext';
 
 function MyProvider({ children }) {
-  const value = {};
+  const [searchedFoods, setSearchedFoods] = useState([]);
+
+  const value = {
+    setSearchedFoods,
+    searchedFoods,
+  };
 
   return (
     <MyContext.Provider value={ value }>
