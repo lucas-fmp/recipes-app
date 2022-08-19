6fc76935e6a6004163e04e459450e6c2fd53780b
Luan Victor <luanvictor2108@gmail.com>
2022-08-19 18:25:21 -0300

    Finalizando tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,69 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isDisabled, setDisabled] = useState(true);
+  const history = useHistory();
+  const minPassword = 5;
+
+  const submitLogin = () => {
+    const userObj = { email };
+    const userObjJSON = JSON.stringify(userObj);
+    localStorage.setItem('user', userObjJSON);
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    history.push('foods');
+  };
+
+  const validateButton = () => {
+    if (
+      email.includes('@')
+      && email.length > 0
+      && email.includes('.')
+      && password.length > minPassword) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
+  const handleChange = ({ target }) => {
+    if (target.name === 'Email') {
+      console.log('funcionou');
+      setEmail(target.value);
+      validateButton();
+    } else {
+      setPassword(target.value);
+      validateButton();
+    }
+  };
+
   return (
     <div>
       <form>
         <input
+          name="Email"
           data-testid="email-input"
           type="email"
           placeholder="Insira seu email"
           value={ email }
-          onChange={ ({ target }) => setEmail(target.value) }
+          onChange={ handleChange }
         />
         <input
+          name="Password"
           data-testid="password-input"
           type="password"
           placeholder="Insira sua senha"
           value={ password }
-          onChange={ ({ target }) => setPassword(target.value) }
+          onChange={ handleChange }
         />
         <button
           type="button"
           data-testid="login-submit-btn"
-          onClick={ () => {} }
+          onClick={ submitLogin }
+          disabled={ isDisabled }
         >
           Enter
 
