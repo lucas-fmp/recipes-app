94e0e2ff956560c383941bdb73af29aaa1315262
Luan Victor <luanvictor2108@gmail.com>
2022-08-19 18:25:21 -0300

    resolving conflicts

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
 import { useHistory } from 'react-router';
-=======
->>>>>>> e174a38 (Resolvendo conflitos)
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-<<<<<<< HEAD
   const [isDisabled, setDisabled] = useState(true);
   const history = useHistory();
   const minPassword = 5;
@@ -18,7 +14,7 @@ function Login() {
     localStorage.setItem('user', userObjJSON);
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
-    history.push('/foods');
+    history.push('foods');
   };
 
   const validateButton = () => {
@@ -43,52 +39,32 @@ function Login() {
     }
   };
 
-=======
->>>>>>> e174a38 (Resolvendo conflitos)
   return (
     <div>
       <form>
         <input
-<<<<<<< HEAD
           name="Email"
-=======
->>>>>>> e174a38 (Resolvendo conflitos)
           data-testid="email-input"
           type="email"
           placeholder="Insira seu email"
           value={ email }
-<<<<<<< HEAD
           onChange={ handleChange }
         />
         <input
           name="Password"
-=======
-          onChange={ ({ target }) => setEmail(target.value) }
-        />
-        <input
->>>>>>> e174a38 (Resolvendo conflitos)
           data-testid="password-input"
           type="password"
           placeholder="Insira sua senha"
           value={ password }
-<<<<<<< HEAD
           onChange={ handleChange }
-=======
-          onChange={ ({ target }) => setPassword(target.value) }
->>>>>>> e174a38 (Resolvendo conflitos)
         />
         <button
           type="button"
           data-testid="login-submit-btn"
-<<<<<<< HEAD
           onClick={ submitLogin }
           disabled={ isDisabled }
-=======
-          onClick={ () => {} }
->>>>>>> e174a38 (Resolvendo conflitos)
         >
           Enter
-
         </button>
       </form>
     </div>
