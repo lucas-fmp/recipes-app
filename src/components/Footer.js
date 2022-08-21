29362c4186d1c7c1c67c4d39b40bc1f9e08b3ac8
JoyceTeofilo <joyceteofilo@gmail.com>
2022-08-20 21:13:46 -0300

    Add Footer - Requisitos 16, 17 e 18

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import '../styles/footer.css';
+
+function Footer() {
+  return (
+
+    <footer data-testid="footer">
+
+      <Link to="/drinks">
+        <img src={ drinkIcon } alt="drink" data-testid="drinks-bottom-btn" />
+      </Link>
+
+      <Link to="/foods">
+        <img src={ mealIcon } alt="meal" data-testid="food-bottom-btn" />
+      </Link>
+
+    </footer>
+
+  );
+}
+
+export default Footer;
