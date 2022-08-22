5a55bcddbdad0159d6834ec50434ac6e09497a93
Lucas Fernando <lucasfernandomacedo13@gmail.com>
2022-08-22 07:22:06 -0300

    Recipes Page - completed

--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -6,17 +6,28 @@ function MyProvider({ children }) {
   const [recipes, setRecipes] = useState([]);
   const [categories, setCategories] = useState([]);
   const [filteredRecipes, setFilteredRecipes] = useState([]);
+<<<<<<< HEAD
   const [searchedFoods, setSearchedFoods] = useState([]);
+=======
+>>>>>>> 27fe9f8 (Recipes Page - completed)
 
   const value = {
     recipes,
     setRecipes,
     categories,
     setCategories,
+<<<<<<< HEAD
     filteredRecipes,
     setFilteredRecipes,
+=======
+<<<<<<< HEAD
+>>>>>>> 27fe9f8 (Recipes Page - completed)
     setSearchedFoods,
     searchedFoods,
+=======
+    filteredRecipes,
+    setFilteredRecipes,
+>>>>>>> fdc2bb5 (Recipes Page - completed)
   };
 
   return (
